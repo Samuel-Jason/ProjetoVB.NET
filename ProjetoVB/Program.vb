89e185daa8a3a689e14e189413c7655585fbb253
Samuel <samuelljason@gmail.com>
2023-09-28 01:47:58 -0300

    teste

--- ProjetoVB/Program.vb
+++ ProjetoVB/Program.vb
@@ -1,18 +1,26 @@
 Imports System
+Imports System.ComponentModel
+Imports System.Windows.Forms
 
 Module Program
     Sub Main(args As String())
 
-        Dim comprimento As Integer
-        Dim palavra As String
+        'MessageBox.Show("Minha Caixa de texto", "Texto do titulo", MessageBoxButtons.OK, MessageBoxIcon.Information)'
 
-        palavra = Console.ReadLine()
-        comprimento = palavra.Length()
+        Dim v1, v2 As Integer
+        Console.WriteLine("Digite um numero:")
+        v1 = Console.ReadLine()
+        Console.WriteLine("Digite um numero:")
+        v2 = Console.ReadLine()
 
-        Console.WriteLine()
-        Console.WriteLine("A palavra {0} digitada tem {1} caracteres",
-                            palavra, comprimento)
-        Console.WriteLine()
+        If v1 = v2 Then
+            Console.WriteLine("Os numeros são iguais")
+        Else
+            Console.WriteLine("Os numeros são diferentes")
+        End If
+
+        Console.ReadLine()
+
 
     End Sub
 End Module
